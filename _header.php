204d968974ba0b698977b03bbe6ac2fc8c9ece3b
Dereckson <dereckson@espace-win.org>
2012-07-23 12:53:31 +0200

    Fixing Pluton kludge to use the current folder and not an hardcoded path.

--- _header.php
+++ _header.php
@@ -1,5 +1,5 @@
 <?php
 	//Kludge to be able to load libraries both in Pluton and in stand-alone tool version.
-	$tool_directory = getcwd() . '/tools/SourceTemplatesGenerator';
+	$tool_directory = getcwd() . '/' . str_replace('-', '/', $document->topic);
 	ini_set('include_path', ini_get('include_path') . ":$tool_directory");
 ?>
