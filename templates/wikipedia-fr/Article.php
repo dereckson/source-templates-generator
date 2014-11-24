c7de6540e7d16977592a150453de5165aa644b8c
Dereckson <dereckson@espace-win.org>
2014-11-24 12:33:42 +0000

    Trims author fields and remove extraneous ponctuation
    Nature adds a semicolon followed by a non breakable space at the end of the
    citation_authors template in the News section of the site.
    
    e.g. <meta name="citation_authors" content="Cressey, Daniel; "/>
    
    These characters are now trimmed from the author field for {{Article}}.
    
    Side edits: fixing whitespace issues and improving code documentation.
    
    Future edits: it could be interesting to move this citation handling code in
    a more generic helper class to allow more specialized article templates.
    
    Fixes T138

--- templates/wikipedia-fr/Article.php
+++ templates/wikipedia-fr/Article.php
@@ -1,182 +1,204 @@
 <?php
+
 setlocale(LC_TIME, 'fr_FR.UTF-8');
 
+/**
+ * Represents an {{Article}} template on the French Wikipedia
+ */
 class ArticleTemplate extends Template {
-	public $lang;
-	public $title;
-	public $periodique;
-	public $year;
-	public $accessdate;
+    public $lang;
+    public $title;
+    public $periodique;
+    public $year;
+    public $accessdate;
 
-	function __construct () {
-		$this->name = "Article";
-		$this->accessdate = trim(strftime(LONG_DATE_FORMAT));
-	}
+    /**
+     * Initializes a new instance of the ArticleTemplate class
+     */
+    function __construct () {
+        $this->name = "Article";
+        $this->accessdate = trim(strftime(LONG_DATE_FORMAT));
+    }
 
-	static function loadFromPage ($page) {
-		$template = new self();
-		$t = $page->meta_tags;
+    /**
+     * Creates a new instance of the ArticleTemplate class from a Page object
+     *
+     * @param Page $page The page to fill the template with data from
+     * @return ArticleTemplate
+     */
+    static function loadFromPage ($page) {
+        $template = new self();
+        $t = $page->meta_tags;
 
-		//Language
-		$template->lang = page::getMetaTag($t, 'dc_language', 'citation_language', 'dc.Language');
+        //Language
+        $template->lang = page::getMetaTag($t, 'dc_language', 'citation_language', 'dc.Language');
 
-		//Authors
-		if ($author = $page->author ?: page::getMetaTag($t, 'author', 'dc_creator', 'citation_authors', 'dc_contributor', 'citation_author', 'dc.Creator', 'DCSext.author')) {
-			//TODO: handle Alpha Beta syntax instead Beta, Alpha
-			$template->authors[] = explode(', ', $author, 2);
-		}
+        //Authors
+        if ($author = $page->author ?: page::getMetaTag($t, 'author', 'dc_creator', 'citation_authors', 'dc_contributor', 'citation_author', 'dc.Creator', 'DCSext.author')) {
+            //TODO: handle Alpha Beta syntax instead Beta, Alpha
+            $author = trim($author, self::getTrimCharacterMask());
+            $template->authors[] = explode(', ', $author, 2);
+        }
 
-		//Title
-		if (!$template->title = page::getMetaTag($t, 'dc_title', 'citation_title')) {
-			$template->title = $page->title;
-		}
+        //Title
+        if (!$template->title = page::getMetaTag($t, 'dc_title', 'citation_title')) {
+            $template->title = $page->title;
+        }
 
-		//Journal, publisher
-		$template->journal =  $page->journal ?: page::getMetaTag($t, 'prism_publicationname', 'citation_journal_title', 'og:site_name');
-		$template->journalLink = $t['dc_source'];
-		$template->publisher = $page->publisher ?: page::getMetaTag($t, 'dc_publisher', 'citation_publisher');
+        //Journal, publisher
+        $template->journal = $page->journal ?: page::getMetaTag($t, 'prism_publicationname', 'citation_journal_title', 'og:site_name');
+        $template->journalLink = $t['dc_source'];
+        $template->publisher = $page->publisher ?: page::getMetaTag($t, 'dc_publisher', 'citation_publisher');
 
-		//Issue name, number and volume
-		$template->issue  = $page->issue ?: page::getMetaTag($t, 'prism_number', 'citation_issue');
-		$template->volume = $page->volume ?: page::getMetaTag($t, 'citation_volume');
-		if (
-			(!$template->issueName = $t['prism_issuename'])
-			&&
-			array_key_exists('dc_relation_ispartof', $t)
-		) {
-			$template->issueName = $t['dc_relation_ispartof']
-			                     . " <!-- !!! paramètre à nettoyer !!! -->";
-		}
+        //Issue name, number and volume
+        $template->issue  = $page->issue ?: page::getMetaTag($t, 'prism_number', 'citation_issue');
+        $template->volume = $page->volume ?: page::getMetaTag($t, 'citation_volume');
+        if (
+            (!$template->issueName = $t['prism_issuename'])
+            &&
+            array_key_exists('dc_relation_ispartof', $t)
+        ) {
+            $template->issueName = $t['dc_relation_ispartof']
+                                 . " <!-- !!! paramètre à nettoyer !!! -->";
+        }
 
-		//Date
-		if ($page->yyyy && $page->mm && $page->dd) {
-			$template->yyyy = $page->yyyy;
-			$template->mm = $page->mm;
-			$template->dd = $page->dd;
-		} elseif ($page->unixtime) {
-			$template->yyyy = date('Y', $page->unixtime);
-			$template->mm   = date('m', $page->unixtime);
-			$template->dd   = date('j', $page->unixtime);
-		} elseif ($date = page::getMetaTag($t, 'prism_publicationdate', 'dc_date', 'citation_date', 'datePublished', 'DC.date.issued')) {
-			if ($date[4] == '/' || $date[4] == '-' || $date[4] == '.') {
-				$template->yyyy = substr($date, 0, 4);
-				$template->mm   = substr($date, 5, 2);
-				$template->dd   = substr($date, 8, 2);
-			} else {
-				$template->yyyy = substr($date, 6, 4);
-				$template->mm   = substr($date, 0, 2);
-				$template->dd   = substr($date, 3, 2);
-			}
-		} else {
-			$template->yyyy = $page->yyyy ?: page::getMetaTag($t, 'citation_year', 'citation_publication_date');
-		}
+        //Date
+        if ($page->yyyy && $page->mm && $page->dd) {
+            $template->yyyy = $page->yyyy;
+            $template->mm = $page->mm;
+            $template->dd = $page->dd;
+        } elseif ($page->unixtime) {
+            $template->yyyy = date('Y', $page->unixtime);
+            $template->mm   = date('m', $page->unixtime);
+            $template->dd   = date('j', $page->unixtime);
+        } elseif ($date = page::getMetaTag($t, 'prism_publicationdate', 'dc_date', 'citation_date', 'datePublished', 'DC.date.issued')) {
+            if ($date[4] == '/' || $date[4] == '-' || $date[4] == '.') {
+                $template->yyyy = substr($date, 0, 4);
+                $template->mm   = substr($date, 5, 2);
+                $template->dd   = substr($date, 8, 2);
+            } else {
+                $template->yyyy = substr($date, 6, 4);
+                $template->mm   = substr($date, 0, 2);
+                $template->dd   = substr($date, 3, 2);
+            }
+        } else {
+            $template->yyyy = $page->yyyy ?: page::getMetaTag($t, 'citation_year', 'citation_publication_date');
+        }
 
-		//Pages
-		if ($page->pages) {
-			$template->pageStart = $page->pages;
-		} else {
-			$template->pageStart = page::getMetaTag($t, 'prism_startingpage', 'citation_firstpage', 'citation_first_page');
-			$template->pageEnd   = page::getMetaTag($t, 'prism_endingpage',   'citation_lastpage',  'citation_last_page');
-		}
+        //Pages
+        if ($page->pages) {
+            $template->pageStart = $page->pages;
+        } else {
+            $template->pageStart = page::getMetaTag($t, 'prism_startingpage', 'citation_firstpage', 'citation_first_page');
+            $template->pageEnd   = page::getMetaTag($t, 'prism_endingpage',   'citation_lastpage',  'citation_last_page');
+        }
 
-		//ISBN, ISSN, URLs
-		$template->issn = $page->issn ?: page::getMetaTag($t, 'prism_issn', 'citation_issn');
-		$template->isbn = page::getMetaTag($t, 'citation_isbn');
-		$template->doi  = $page->doi ?: page::getMetaTag($t, 'citation_doi');
+        //ISBN, ISSN, URLs
+        $template->issn = $page->issn ?: page::getMetaTag($t, 'prism_issn', 'citation_issn');
+        $template->isbn = page::getMetaTag($t, 'citation_isbn');
+        $template->doi  = $page->doi ?: page::getMetaTag($t, 'citation_doi');
 
-		$template->summary = page::getMetaTag($t, 'citation_abstract_html_url');
-		$template->url = self::getTextURL($page->url, $t);
+        $template->summary = page::getMetaTag($t, 'citation_abstract_html_url');
+        $template->url = self::getTextURL($page->url, $t);
 
-		return $template;
-	}
+        return $template;
+    }
 
-	function __toString () {
-		//Langue
-		$this->params['langue'] = $this->lang;
+    function __toString () {
+        //Langue
+        $this->params['langue'] = $this->lang;
 
-		//Auteur
-		if (!count($this->authors)) {
-			//Per http://fr.wikipedia.org/w/?&diff=93455862, print
+        //Auteur
+        if (!count($this->authors)) {
+            //Per http://fr.wikipedia.org/w/?&diff=93455862, print
                         //one blank set of lines for author when the article
                         //metadata doesn't offer author information.
-			$this->authors = [['', '']];
-		}
-		$k = 1;
-		foreach ($this->authors as $author) {
-			$this->params["prénom$k"] = $author[1];
-			$this->params["nom$k"] = $author[0];
-			$this->params["lien auteur$k"] = '';
-			$k++;
-		}
+            $this->authors = [['', '']];
+        }
+        $k = 1;
+        foreach ($this->authors as $author) {
+            $this->params["prénom$k"] = $author[1];
+            $this->params["nom$k"] = $author[0];
+            $this->params["lien auteur$k"] = '';
+            $k++;
+        }
 
-		//Titre, périodique, éditeur, volume, etc.
-		$this->params['titre'] = $this->title;
-		$this->params['périodique'] = $this->journal;
-		//TODO: vérifier si l'aticle existe sur fr.wikip et contient l'infobox Presse ou est rattaché à une catégorie fille de [[Catégorie:Revue scientifique]]
-		//$this->params['lien périodique'] = $this->journal;
-		$this->params['éditeur'] = $this->publisher;
-		if ($this->volume) $this->params['volume'] = $this->volume;
-		$this->params['numéro'] = $this->issue;
-		if ($this->issueName) $this->params['titre numéro'] = $this->issueName;
+        //Titre, périodique, éditeur, volume, etc.
+        $this->params['titre'] = $this->title;
+        $this->params['périodique'] = $this->journal;
+        //TODO: vérifier si l'aticle existe sur fr.wikip et contient l'infobox Presse ou est rattaché à une catégorie fille de [[Catégorie:Revue scientifique]]
+        //$this->params['lien périodique'] = $this->journal;
+        $this->params['éditeur'] = $this->publisher;
+        if ($this->volume) $this->params['volume'] = $this->volume;
+        $this->params['numéro'] = $this->issue;
+        if ($this->issueName) $this->params['titre numéro'] = $this->issueName;
 
-		//Date
-		if ($this->mm && $this->dd) {
-			$date = mktime(12, 0, 0, (int)$this->mm, (int)$this->dd, (int)$this->yyyy);
-			$this->params['jour'] = trim(strftime('%e', $date));
-			$this->params['mois'] = strftime('%B', $date);
-		}
-		$this->params['année'] = $this->yyyy;
+        //Date
+        if ($this->mm && $this->dd) {
+            $date = mktime(12, 0, 0, (int)$this->mm, (int)$this->dd, (int)$this->yyyy);
+            $this->params['jour'] = trim(strftime('%e', $date));
+            $this->params['mois'] = strftime('%B', $date);
+        }
+        $this->params['année'] = $this->yyyy;
 
-		//Pages, ISSN, ISBN, DOI, URL, consulté le
-		$this->params['pages'] = $this->pageEnd ? ($this->pageStart . '-' . $this->pageEnd) : $this->pageStart;
-		if ($this->issn) $this->params['ISSN'] = $this->issn;
-		if ($this->isbn) $this->params['ISBN'] = $this->isbn;
-		if ($this->doi) $this->params['doi'] = $this->doi;
-		$this->params['url texte'] = $this->url;
- 		if (self::isSummaryPertinent($this->url, $this->summary)) {
-			$this->params['résumé'] = $this->summary;
-		}
-		$this->params['consulté le'] = trim(strftime(LONG_DATE_FORMAT));
+        //Pages, ISSN, ISBN, DOI, URL, consulté le
+        $this->params['pages'] = $this->pageEnd ? ($this->pageStart . '-' . $this->pageEnd) : $this->pageStart;
+        if ($this->issn) $this->params['ISSN'] = $this->issn;
+        if ($this->isbn) $this->params['ISBN'] = $this->isbn;
+        if ($this->doi) $this->params['doi'] = $this->doi;
+        $this->params['url texte'] = $this->url;
+        if (self::isSummaryPertinent($this->url, $this->summary)) {
+            $this->params['résumé'] = $this->summary;
+        }
+        $this->params['consulté le'] = trim(strftime(LONG_DATE_FORMAT));
 
-		return parent::__toString();
-	}
+        return parent::__toString();
+    }
 
-	/**
-	 * Gets article full text URL
-         *
-         * @param string $url the article current URL
-	 *
-	 * @return string the article fulltext URL
-	 */
-	static function getTextURL ($url, $metatags) {
-		if (strpos($url, '.revues.org/') > 0) {
-			//revues.org PDF generation is broken
-			return $url;
-		}
+    /**
+     * Gets article full text URL
+     *
+     * @param string $url the article current URL
+     *
+     * @return string the article fulltext URL
+     */
+    static function getTextURL ($url, $metatags) {
+        if (strpos($url, '.revues.org/') > 0) {
+            //revues.org PDF generation is broken
+            return $url;
+        }
 
-		if ($text_url = page::getMetaTag($metatags, 'citation_pdf_url', 'citation_fulltext_html_url')) {
-			return $text_url;
-		}
+        if ($text_url = page::getMetaTag($metatags, 'citation_pdf_url', 'citation_fulltext_html_url')) {
+            return $text_url;
+        }
 
-		return $url;
-	}
+        return $url;
+    }
 
-        /**
-         * Determines if a summary is pertinent to include in parameters
-	 *
-	 * @param string $url_article Article URL
-	 * @param string $url_summary Summary URL
-	 *
-	 * @return bool true if the summary URL should be included in templat ; otherwise, false
-         */
-	static function isSummaryPertinent ($url_article, $url_summary) {
-		//Empty summary or identical to URL are rejected
-		if ($url_summary == '' || $url_summary == $url_article) return false;
+    /**
+     * Determines if a summary is pertinent to include in parameters
+     *
+     * @param string $url_article Article URL
+     * @param string $url_summary Summary URL
+     *
+     * @return bool true if the summary URL should be included in templat ; otherwise, false
+     */
+    static function isSummaryPertinent ($url_article, $url_summary) {
+        //Empty summary or identical to URL are rejected
+        if ($url_summary == '' || $url_summary == $url_article) return false;
 
-		//This site is indexed through /resume.php but gives /article.php as summary URL in metadata
-		if (substr($url_article, 0, 32) == "http://www.cairn.info/resume.php") return false;
+        //This site is indexed through /resume.php but gives /article.php as summary URL in metadata
+        if (substr($url_article, 0, 32) == "http://www.cairn.info/resume.php") return false;
 
-		return true;
-	}
+        return true;
+    }
+
+    /**
+     * Gets a trim character mask to clean pure alphanumeric data fields from extraneous ponctuation
+     *
+     * @return a character mask to use for trim() avoiding whitespaces and some other ponctuation
+     */
+    static function getTrimCharacterMask() {
+        return "\t\n\r\0\x0B,; ";
+    }
 }
-?>
