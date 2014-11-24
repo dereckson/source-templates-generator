1882d15c3245d1d5600ad26d41ff92b2296b89bf
Dereckson <dereckson@espace-win.org>
2014-11-24 00:56:14 +0000

    Page::grab $after parameter is now facultative

--- page.php
+++ page.php
@@ -280,16 +280,26 @@ class Page {
      *
      * @param string $text   The text where to find the substring
      * @param string $before The string at the left  of the text to be grabbed
-     * @param string $after  The string at the right of the text to be grabbed
+     * @param string $after  The string at the right of the text to be grabbed [facultative]
      *
      * @return string The text found between $before and $after
      */
-    static function grab ($text, $before, $after) {
+    static function grab ($text, $before, $after = null) {
         $pos1 = strpos($text, $before);
-        if ($pos1 === false) { return false; } else { $pos1 += strlen($before); }
+        if ($pos1 === false) {
+            return false;
+        } else {
+            $pos1 += strlen($before);
+        }
+
+        if ($after === null) {
+            return substr($text, $pos1);
+        }
 
         $pos2 = strpos($text, $after, $pos1 + 1);
-        if ($pos2 === false) { return false; }
+        if ($pos2 === false) {
+            return false;
+        }
 
         return substr($text, $pos1, $pos2 - $pos1);
     }
