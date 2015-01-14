9695926b88c5c5baa0ab641d4a65618d07a6a9d6
Sébastien Santoro <dereckson@espace-win.org>
2015-01-14 17:23:11 +0000

    Fixed code style issues

--- _header.php
+++ _header.php
@@ -1,5 +1,4 @@
 <?php
-	//Kludge to be able to load libraries both in Pluton and in stand-alone tool version.
-	$tool_directory = getcwd() . '/' . str_replace('-', '/', $document->topic);
-	ini_set('include_path', ini_get('include_path') . ":$tool_directory");
-?>
+    //Kludge to be able to load libraries both in Pluton and in stand-alone tool version.
+    $tool_directory = getcwd() . '/' . str_replace('-', '/', $document->topic);
+    ini_set('include_path', ini_get('include_path') . ":$tool_directory");
