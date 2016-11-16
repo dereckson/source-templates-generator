a8eca3596b28fbdffada68e1690e7ca32ce6891d
Sébastien Santoro <dereckson@espace-win.org>
2016-11-16 17:10:27 +0000

    Improve Libération analyze
    Summary:
      - Extracts author from news article
      - Identify the site as behind a paywall
    
    Ref T1049.
    
    Reviewers: dereckson
    
    Maniphest Tasks: T1049
    
    Differential Revision: https://devcentral.nasqueron.org/D728

--- pages/liberation.php
+++ pages/liberation.php
@@ -4,14 +4,19 @@ class LiberationPage extends Page {
     function analyse () {
         parent::analyse();
 
-        //Hardcoded known info
+        // Hardcoded known info
         $this->site = "[[Libération (journal)|Libération]]";
+        $this->paywall = true;
 
-        //Gets date
+        // Gets date
         // e.g. http://www.liberation.fr/france/2016/06/02/affiches...
         $pos = strpos($this->url, "/20");
         $this->yyyy = substr($this->url, $pos + 1, 4);
         $this->mm   = substr($this->url, $pos + 6, 2);
         $this->dd   = substr($this->url, $pos + 9, 2);
+
+        // Extracts author
+        $author = self::between('<span class="author">', '</span>');
+        $this->author = self::grab($author, '<span>'); // cleans URL
     }
 }
