451be0c795e6a3883b715c01c534382cab009cc6
Sébastien Santoro <dereckson@espace-win.org>
2016-11-17 01:53:51 +0000

    Analyze next.liberation.fr
    Summary:
      - fix URL date extraction to allow 199x articles
      - some authors can use an <a> instead of a <span> element
    
    Test Plan:
    Change tested with the following URLs:
    
      - http://next.liberation.fr/livres/1996/05/09/christian-jacq-le-nouveau-pharaon_172245
      - http://www.liberation.fr/france/2016/11/15/feuilleton-takieddine-le-retour-si-vous-avez-rate-le-debut_1528493
      - http://www.liberation.fr/france/2016/10/18/bagelstein-les-peines-de-prison-ferme-de-militants-feministes-commuees-en-sursis_1522841
    
    Reviewers: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D731

--- pages/liberation.php
+++ pages/liberation.php
@@ -10,13 +10,14 @@ class LiberationPage extends Page {
 
         // Gets date
         // e.g. http://www.liberation.fr/france/2016/06/02/affiches...
-        $pos = strpos($this->url, "/20");
-        $this->yyyy = substr($this->url, $pos + 1, 4);
-        $this->mm   = substr($this->url, $pos + 6, 2);
-        $this->dd   = substr($this->url, $pos + 9, 2);
+        $this->extractYYYYMMDDDateFromURL();
 
         // Extracts author
         $author = self::between('<span class="author">', '</span>');
         $this->author = self::grab($author, '<span>'); // cleans URL
+        if ($this->author === false) {
+            $this->author = self::grab($author, '">', '</a>'); // link
+        }
+        $this->author = trim($this->author);
     }
 }
